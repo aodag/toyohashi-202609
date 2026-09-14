f7a4155de6855738d87dd130a12034bfe87d590d
Atsushi Odagiri <aodagx@gmail.com>
2026-09-14 16:38:54 +0900

    gi poppler

--- greader.py
+++ greader.py
@@ -7,4 +7,12 @@ gi.require_version("Poppler", "0.18")
 from gi.repository import Gtk, Poppler
 
 
-print(f"{Gtk=}, {Poppler=}")
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file")
+args = parser.parse_args()
+filename = args.file
+print(f"{filename=}")
+doc = Poppler.Document.new_from_file(filename, None)
+print(f"{doc.get_n_pages()=}")
